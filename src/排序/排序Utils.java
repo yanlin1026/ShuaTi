fa7d4e61b8e87f66a208f40be805750a041bda20
yanlin1026 <244679136@qq.com>
2018-08-17 19:28:52 +0800

    排序算法

--- src/排序/排序Utils.java
+++ src/排序/排序Utils.java
@@ -6,7 +6,7 @@ package 排序;
  */
 public class 排序Utils {
 
-    public static void 打印数组(int[] a) {
+    public static void display(int[] a) {
 
         for (int i : a) {
             System.out.print(i + " ");
@@ -20,7 +20,7 @@ public class 排序Utils {
         a[j] = tmp;
     }
 
-    public static void 冒泡(int[] a) {
+    public static void bubbleSort(int[] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = a.length - 1; j > i; j--) {
                 if (a[j] < a[j - 1]) {
@@ -28,7 +28,7 @@ public class 排序Utils {
                 }
             }
         }
-        打印数组(a);
+        display(a);
     }
 
     /**
@@ -37,7 +37,7 @@ public class 排序Utils {
      *
      * @param a
      */
-    public static void 选择(int[] a) {
+    public static void selectSort(int[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j <= a.length - 1; j++) {
@@ -47,7 +47,7 @@ public class 排序Utils {
             }
             交换(a, i, minIndex);
         }
-        打印数组(a);
+        display(a);
     }
 
     /**
@@ -57,7 +57,7 @@ public class 排序Utils {
      *
      * @param a
      */
-    public static void 插入(int[] a) {
+    public static void insertSort(int[] a) {
         for (int i = 1; i <= a.length - 1; i++) {
             for (int j = i - 1; j >= 0; j--) {
                 if (a[j] > a[j + 1]) {
@@ -65,12 +65,12 @@ public class 排序Utils {
                 }
             }
         }
-        打印数组(a);
+        display(a);
     }
 
     /**
      * 将大于第一个数的全部放右边，小于第一个数的全部放左边
-     *
+     * 荷兰国旗问题，快排思想
      * @param a
      */
     public static void 荷兰国旗问题(int[] a) {
@@ -87,6 +87,65 @@ public class 排序Utils {
                 i++;
             }
         }
-        打印数组(a);
+        display(a);
+    }
+
+    public static void quickSort(int[] a, int left, int right) {
+        if (left > right)
+            return;
+
+        int i = left;
+        int j = right;
+        int tmp = a[left];
+        while (i != j) {
+            while (a[j] >= tmp && i < j) {
+                j--;
+            }
+            while (a[i] <= tmp && i < j) {
+                i++;
+            }
+            if (i < j) {
+                交换(a, i, j);
+            }
+        }
+        //基数tmp归位
+        a[left] = a[i];
+        a[i] = tmp;
+        quickSort(a, left, i - 1);
+        quickSort(a, i + 1, right);
+
+    }
+
+    public static void mergeSort(int[] a, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int mid = left + ((right - left) >> 1);
+        mergeSort(a, left, mid);
+        mergeSort(a, mid + 1, right);
+        merge(a, left, mid, right);
+    }
+
+    public static void merge(int[] a, int left, int middle, int right) {
+        //辅助队列
+        int[] help = new int[right - left + 1];
+        int i = 0;
+        int p1 = left;
+        int p2 = middle + 1;
+        while (p1 <= middle && p2 <= right) {
+            help[i++] = a[p1] < a[p2] ? a[p1++] : a[p2++];
+        }
+        //上面循环结束的时，p1=middle或p2=right
+        while (p2 <= right) {//p1 = middle
+            help[i++] = a[p2++];
+        }
+        while (p1 <= middle) {//p2 = right
+            help[i++] = a[p1++];
+        }
+
+        //将辅助的数组赋值给原数组
+        for (i = 0; i < help.length; i++) {
+            a[left + i] = help[i];
+        }
     }
 }
