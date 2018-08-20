e02a72db73ad609084939914bb6b9e551c315ff2
yanlin1026 <244679136@qq.com>
2018-08-20 19:48:56 +0800

    背包学习

--- src/真题练习/背包问题.java
+++ src/真题练习/背包问题.java
@@ -1,7 +1,6 @@
 package 真题练习;
 
 import static Utils.Utils.display;
-import static com.sun.glass.ui.gtk.GtkApplication.display;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,38 +9,42 @@ import static com.sun.glass.ui.gtk.GtkApplication.display;
  * Description:
  */
 public class 背包问题 {
-    public static void main(String[] args) {
-        int[] weight = {3, 5, 2, 6, 4}; //物品重量
-        int[] val = {4, 4, 3, 5, 3}; //物品价值
-        int m = 12; //背包容量
-        int n = val.length; //物品个数
 
-        int[][] dp = new int[n + 1][m + 1]; //dp[i][j]表示前i个物品能装入容量为j的背包中的最大价值
-        int[][] path = new int[n + 1][m + 1];
+    /**
+     * 01背包问题
+     *
+     * @param weight：商品的重量
+     * @param value：商品的价值
+     * @param capacity：背包的容量
+     */
+    public static void packageBag(int[] weight, int[] value, int capacity) {
+        int n = value.length;//商品的个数
+        int[][] dp = new int[n + 1][capacity + 1]; //dp[i][j]表示前i个物品能装入容量为j的背包中的最大价值
+        int[][] path = new int[n + 1][capacity + 1];
         //初始化第一列和第一行
         for (int i = 0; i < dp.length; i++) {
-            dp[i][0] = 0;
+            dp[i][0] = 0;// 如果容量j==0；意味着无法选择商品
         }
         for (int i = 0; i < dp[0].length; i++) {
-            dp[0][i] = 0;
+            dp[0][i] = 0;// 如果商品个数为0，同样意味着无法选择商品
         }
-        //通过公式迭代计算
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                if (weight[i - 1] > j)
-                    dp[i][j] = dp[i - 1][j];
-                else {
-                    if (dp[i - 1][j] < dp[i - 1][j - weight[i - 1]] + val[i - 1]) {
-                        dp[i][j] = dp[i - 1][j - weight[i - 1]] + val[i - 1];
+        for (int i = 1; i < dp.length; i++) {// i表示商品的下表
+            for (int j = 1; j < dp[0].length; j++) {//其实j表示背包的剩余重量
+                if (weight[i - 1] > j)//如果i-1位置的重量大于背包的剩余容量j
+                    dp[i][j] = dp[i - 1][j]; //因此，商品选择只能从前i-1中选取
+                else { // j-weight[i-1]：表示选择j时之前剩余的容量
+                    if (dp[i - 1][j] < dp[i - 1][j - weight[i - 1]] + value[i - 1]) {
+                        dp[i][j] = dp[i - 1][j - weight[i - 1]] + value[i - 1];
                         path[i][j] = 1;
                     } else {
                         dp[i][j] = dp[i - 1][j];
                     }
-                    //dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i-1]]+val[i-1]);
+                    //dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i-1]]+value[i-1]);
                 }
             }
         }
-        display("dp数组为：",dp);
+        display("dp数组为：", dp);
+        display("path数组为：", path);
 
         int i = dp.length - 1;
         int j = dp[0].length - 1;
@@ -52,6 +55,37 @@ public class 背包问题 {
             }
             i--;
         }
+    }
+
+    /**
+     * 完全背包，商品可以多次选择
+     * @param weight
+     * @param value
+     * @param capacity
+     */
+    public static void packageFullBag(int[] weight, int[] value, int capacity) {
+        int n = value.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] weight = {3, 5, 2, 6, 4}; //物品重量
+        int[] value = {4, 4, 3, 5, 3}; //物品价值
+
+        packageBag(weight, value, 12);
+
 
     }
 
