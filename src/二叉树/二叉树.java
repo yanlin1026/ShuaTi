18dae29a149eefbc5090ac148fab43fbee497954
yanlin <244679136@qq.ocm>
2018-08-29 17:56:38 +0800

    0827美团面试 修改部分代码

---
+++ src/二叉树/二叉树.java
@@ -0,0 +1,13 @@
+package 二叉树;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yanlin
+ * Date: 2018/8/23 11:37
+ * Description:
+ */
+public class 二叉树 {
+    public static void main(String[] args) {
+        System.err.println(2);
+    }
+}
--- src/真题练习/字符串全排列.java
+++ src/真题练习/字符串全排列.java
@@ -4,13 +4,12 @@ package 真题练习;
  * Created with IntelliJ IDEA.
  * User: yanlin
  * Date: 2018/8/19 23:13
- * Description:
+ * Description:字符串09:23-23:50
  */
 public class 字符串全排列 {
 
 
-
-    public static char[] 字符交换(char[] a, int i, int j){
+    public static char[] 字符交换(char[] a, int i, int j) {
 
         char t = a[i];
         a[i] = a[j];
@@ -18,47 +17,73 @@ public class 字符串全排列 {
         return a;
     }
 
-    public static void 字符串全排列(char[] c, int from, int to){
-        if(from == to){
-            for(int i = 0;i <= to; i++){
+    /**
+     * @param c
+     * @param from
+     * @param to
+     * @Description: 字符串09:23-23:50
+     *
+     */
+    public static void 字符串全排列(char[] c, int from, int to) {
+        if (from == to) {
+            for (int i = 0; i <= to; i++) {
                 System.out.print(c[i]);
             }
             System.out.println();
         }
-        for(int i = from; i <= to; i++){
-            字符交换(c,i,from);
-            字符串全排列(c,from+1,to);
-            字符交换(c,i,from);
+        for (int i = from; i <= to; i++) {
+            字符交换(c, i, from);//第一次,第一个数跟自己交换
+            字符串全排列(c, from + 1, to);
+            字符交换(c, i, from);
         }
     }
 
-    public static void 字符串包含重复数字全排列(char[] c, int from, int to){
-        if(from == to){
-            for(int i = 0;i <= to; i++){
+    /**
+     * @param c
+     * @param from
+     * @param to
+     * @Description: 字符串24:00-
+     */
+    public static void 字符串包含重复数字全排列(char[] c, int from, int to) {
+        if (from == to) {
+            for (int i = 0; i <= to; i++) {
                 System.out.print(c[i]);
             }
             System.out.println();
         }
 
-        for(int i = from; i <= to; i++){
+        for (int i = from; i <= to; i++) {
 
-            boolean bool = true;
-            for(int j = from; j < i; j++){
-                if(c[i]==c[j]){
-                    bool = false;
-                    break;
-                }
-            }
-            if(bool == false)
+
+            if (isSwap(c, from, i) == false)
                 continue;
-            字符交换(c,i,from);
-            字符串包含重复数字全排列(c,from+1,to);
-            字符交换(c,i,from);
+            字符交换(c, i, from);
+            字符串包含重复数字全排列(c, from + 1, to);
+            字符交换(c, i, from);
         }
     }
+
+    /**
+     * @param c
+     * @param from
+     * @param to
+     * @return
+     *  判断，如果有重复字符，
+     */
+    public static boolean isSwap(char[] c, int from, int to) {
+        boolean bool = true;
+        for (int i = from; i < to; i++) {
+            if (c[i] == c[to]) {
+                bool = false;
+                break;
+            }
+        }
+        return bool;
+    }
+
     public static void main(String[] args) {
-        char[] c1 = {'1','2','3'};
-        字符串全排列(c1, 0, c1.length-1);
+        char[] c1 = {'1', '2', '2', '3'};
+        字符串全排列(c1, 0, c1.length - 1);
         //字符串包含重复数字全排列(c1, 0, c1.length-1);
 
     }
